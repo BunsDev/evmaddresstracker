4f50585d7682504a305f00a1525e6cd5e134d226
farque65 <fahim.farque@gmail.com>
2024-03-10 21:53:53 -0400

    remove, add, edit addresses

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -8,7 +8,7 @@ import {
 } from 'eth-hooks';
 import { useExchangeEthPrice } from 'eth-hooks/dapps/dex';
 import React, { Fragment, useCallback, useEffect, useState } from 'react';
-import { Link, Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import './App.css';
 import {
   Account,
@@ -26,21 +26,12 @@ import { ALCHEMY_KEY, NETWORKS } from './constants';
 import externalContracts from './contracts/external_contracts';
 // contracts
 import deployedContracts from './contracts/hardhat_contracts.json';
-import { classNames, Transactor, Web3ModalSetup } from './helpers';
+import { Transactor, Web3ModalSetup } from './helpers';
 import { useStaticJsonRPC } from './hooks';
-import { EvmAddressTracker, ExampleUI, Hints, Home, Subgraph } from './views';
+import { EvmAddressTracker } from './views';
 
 import { Dialog, Transition } from '@headlessui/react';
-import {
-  CodeIcon,
-  CurrencyDollarIcon,
-  HomeIcon,
-  MenuAlt2Icon,
-  ShareIcon,
-  SparklesIcon,
-  TemplateIcon,
-  XIcon
-} from '@heroicons/react/outline';
+import { CodeIcon, MenuAlt2Icon, TemplateIcon, XIcon } from '@heroicons/react/outline';
 
 const { ethers } = require('ethers');
 /*
@@ -257,13 +248,8 @@ function App(props) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const navigation = [
-    { name: 'Home', href: '/', icon: HomeIcon },
     { name: 'Debug Contracts', href: '/debug', icon: CodeIcon },
-    { name: 'Hints', href: '/hints', icon: SparklesIcon },
-    { name: 'ExampleUI', href: '/exampleui', icon: TemplateIcon },
     { name: 'EvmAddressTracker', href: '/evmaddresstracker', icon: TemplateIcon },
-    { name: 'Mainnet DAI', href: '/mainnetdai', icon: CurrencyDollarIcon },
-    { name: 'Subgraph', href: '/subgraph', icon: ShareIcon },
   ];
 
   return (
@@ -315,34 +301,6 @@ function App(props) {
                 <div className="flex-shrink-0 flex items-center px-4">
                   <Header />
                 </div>
-                <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                  <nav className="px-2 space-y-1">
-                    {navigation.map(item => {
-                      const current = location.pathname === item.href;
-                      return (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          className={classNames(
-                            current
-                              ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white'
-                              : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white',
-                            'group flex items-center px-2 py-2 text-base font-medium rounded-md',
-                          )}
-                        >
-                          <item.icon
-                            className={classNames(
-                              current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                              'mr-4 flex-shrink-0 h-6 w-6',
-                            )}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </Link>
-                      );
-                    })}
-                  </nav>
-                </div>
                 <div className="flex-shrink-0 flex p-4">
                   <ThemeSwitch />
                 </div>
@@ -355,48 +313,13 @@ function App(props) {
         </Transition.Root>
 
         {/* Static sidebar for desktop */}
-        <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-col flex-grow border-r border-gray-200 dark:border-gray-700 pt-5 bg-white dark:bg-gray-800 overflow-y-auto">
-            <div className="flex items-center flex-shrink-0 px-4">
-              <Header />
-            </div>
-            <div className="mt-5 flex-grow flex flex-col">
-              <nav className="flex-1 px-2 pb-4 space-y-1">
-                {navigation.map(item => {
-                  const current = location.pathname === item.href;
-                  return (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      className={classNames(
-                        current
-                          ? 'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white'
-                          : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white',
-                        'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
-                      )}
-                    >
-                      <item.icon
-                        className={classNames(
-                          current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                          'mr-3 flex-shrink-0 h-6 w-6',
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </Link>
-                  );
-                })}
-              </nav>
-            </div>
-            <div className="flex-shrink-0 flex p-4">
-              <ThemeSwitch />
-            </div>
-          </div>
-        </div>
-        <div className="md:pl-64 flex flex-col flex-1">
+        <div className="flex flex-col flex-1">
           {/* Top nav */}
           <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white dark:bg-gray-800 shadow dark:shadow-gray-700">
+            <div className="flex-1 px-4 flex flex-row">
+              <Header />
+              <ThemeSwitch />
+            </div>
             <button
               type="button"
               className="px-4 border-r border-gray-200 dark:border-gray-700 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
@@ -433,36 +356,7 @@ function App(props) {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <Switch>
                   <Route exact path="/">
-                    {/* pass in any web3 props to this Home component. For example, yourLocalBalance */}
-                    <Home yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
-                  </Route>
-                  <Route exact path="/debug">
-                    {/*
-                          🎛 this scaffolding is full of commonly used components
-                          this <Contract/> component will automatically parse your ABI
-                          and give you a form to interact with it locally
-                      */}
-
-                    <Contract
-                      name="YourContract"
-                      price={price}
-                      signer={userSigner}
-                      provider={localProvider}
-                      address={address}
-                      blockExplorer={blockExplorer}
-                      contractConfig={contractConfig}
-                    />
-                  </Route>
-                  <Route path="/hints">
-                    <Hints
-                      address={address}
-                      yourLocalBalance={yourLocalBalance}
-                      mainnetProvider={mainnetProvider}
-                      price={price}
-                    />
-                  </Route>
-                  <Route path="/exampleui">
-                    <ExampleUI
+                    <EvmAddressTracker
                       address={address}
                       userSigner={userSigner}
                       mainnetProvider={mainnetProvider}
@@ -473,6 +367,17 @@ function App(props) {
                       writeContracts={writeContracts}
                       readContracts={readContracts}
                       purpose={purpose}
+                    />
+                  </Route>
+                  <Route exact path="/debug">
+                    <Contract
+                      name="YourContract"
+                      price={price}
+                      signer={userSigner}
+                      provider={localProvider}
+                      address={address}
+                      blockExplorer={blockExplorer}
+                      contractConfig={contractConfig}
                     />
                   </Route>
                   <Route path="/evmaddresstracker">
@@ -489,73 +394,47 @@ function App(props) {
                       purpose={purpose}
                     />
                   </Route>
-                  <Route path="/mainnetdai">
-                    <Contract
-                      name="DAI"
-                      customContract={mainnetContracts && mainnetContracts.contracts && mainnetContracts.contracts.DAI}
-                      signer={userSigner}
-                      provider={mainnetProvider}
-                      address={address}
-                      blockExplorer="https://etherscan.io/"
-                      contractConfig={contractConfig}
-                      chainId={1}
-                    />
-                    {/*
-                      <Contract
-                        name="UNI"
-                        customContract={mainnetContracts && mainnetContracts.contracts && mainnetContracts.contracts.UNI}
-                        signer={userSigner}
-                        provider={mainnetProvider}
-                        address={address}
-                        blockExplorer="https://etherscan.io/"
-                      />
-                      */}
-                  </Route>
-                  <Route path="/subgraph">
-                    <Subgraph
-                      subgraphUri={props.subgraphUri}
-                      tx={tx}
-                      writeContracts={writeContracts}
-                      mainnetProvider={mainnetProvider}
-                    />
-                  </Route>
                 </Switch>
               </div>
             </div>
           </main>
         </div>
         {/* 🗺 Extra UI like gas price, eth price, faucet, and support: */}
-        <div className="absolute bottom-6 right-6">
-          <div className="mb-1 space-x-2">
-            <Ramp price={price} address={address} networks={NETWORKS} />
-            <GasGauge gasPrice={gasPrice} />
-            <a
-              href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
-              className="inline-flex items-center px-3 py-0.5 rounded-full text-base font-normal bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white"
-            >
-              💬 Support
-            </a>
+        {process.env.REACT_APP_DEBUGGING && (
+          <div className="absolute bottom-6 right-6">
+            <div className="mb-1 space-x-2">
+              <Ramp price={price} address={address} networks={NETWORKS} />
+              <GasGauge gasPrice={gasPrice} />
+              <a
+                href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
+                className="inline-flex items-center px-3 py-0.5 rounded-full text-base font-normal bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-white"
+              >
+                💬 Support
+              </a>
+            </div>
+            {/* if the local provider has a signer, let's show the faucet: */}
+            {faucetAvailable && <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />}
           </div>
-          {/* if the local provider has a signer, let's show the faucet: */}
-          {faucetAvailable && <Faucet localProvider={localProvider} price={price} ensProvider={mainnetProvider} />}
-        </div>
+        )}
       </div>
       {/* Global notification live region, render this permanently at the end of the document */}
-      <div aria-live="assertive" className="fixed inset-0 flex items-start px-4 pt-20 pb-6 pointer-events-none">
-        <div className="w-full flex flex-col items-end space-y-4">
-          {/* Alert if wrong network is selected */}
-          <NetworkDisplay
-            NETWORKCHECK={NETWORKCHECK}
-            localChainId={localChainId}
-            selectedChainId={selectedChainId}
-            targetNetwork={targetNetwork}
-          />
+      {process.env.REACT_APP_DEBUGGING && (
+        <div aria-live="assertive" className="fixed inset-0 flex items-start px-4 pt-20 pb-6 pointer-events-none">
+          <div className="w-full flex flex-col items-end space-y-4">
+            {/* Alert if wrong network is selected */}
+            <NetworkDisplay
+              NETWORKCHECK={NETWORKCHECK}
+              localChainId={localChainId}
+              selectedChainId={selectedChainId}
+              targetNetwork={targetNetwork}
+            />
 
-          {yourLocalBalance.lte(ethers.BigNumber.from('0')) && (
-            <FaucetHint localProvider={localProvider} targetNetwork={targetNetwork} address={address} />
-          )}
+            {yourLocalBalance.lte(ethers.BigNumber.from('0')) && (
+              <FaucetHint localProvider={localProvider} targetNetwork={targetNetwork} address={address} />
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
--- packages/react-app/src/views/EvmAddressTracker.jsx
+++ packages/react-app/src/views/EvmAddressTracker.jsx
@@ -1,10 +1,9 @@
 // @ts-nocheck
-import { SyncOutlined } from '@ant-design/icons';
-import { Button, Card, DatePicker, Divider, Input, Progress, Slider, Spin, Switch } from 'antd';
+import { Button, Divider, Input } from 'antd';
 import { utils } from 'ethers';
 import React, { useEffect, useState } from 'react';
 
-import { Address, Balance } from '../components';
+import { AddressListDisplay, Balance } from '../components';
 
 export default function ExampleUI({
   purpose,
@@ -19,37 +18,99 @@ export default function ExampleUI({
 }) {
   const [newAddress, setNewAddress] = useState('');
   const [addressList, setAddressList] = useState([]);
+  const [editStates, setEditStates] = useState({});
 
   useEffect(() => {
     if (localStorage?.getItem('evmAT')) {
-      setAddressList(localStorage.getItem('evmAT'));
+      const result = localStorage.getItem('evmAT') ? JSON.parse(localStorage.getItem('evmAT')) : null;
+      console.log('result', result);
+      if (result) {
+        setAddressList(result[address]);
+      }
     }
-  }, [addressList, address]);
+  }, []);
+
+  // useEffect(() => {
+  //   if (localStorage?.getItem('evmAT')) {
+  //     const result = localStorage.getItem('evmAT') ? JSON.parse(localStorage.getItem('evmAT')) : null;
+  //     if (result) {
+  //       setAddressList(result[address]);
+  //     }
+  //   }
+  // }, [address, addressList]);
+
+  // Toggle edit state
+  const toggleEdit = address => {
+    setEditStates({
+      ...editStates,
+      [address]: !editStates[address],
+    });
+  };
+
+  //remove address from address list
+  const removeAddress = address => {
+    console.log('removeAddress', address);
+    const oldPayload = JSON.parse(localStorage.getItem('evmAT'));
+    const payload = oldPayload.address.filter(item => item !== address) || [];
+    localStorage.setItem('evmAT', JSON.stringify(payload));
+    setAddressList(oldPayload.address.filter(item => item !== address));
+  };
+
+  //edit address from address list
+  const editAddress = (address, newAddress) => {
+    console.log('editAddress', address, newAddress);
+    const oldPayload = JSON.parse(localStorage.getItem('evmAT'));
+    const payload = oldPayload.address.map(item => (item === address ? newAddress : item));
+    localStorage.setItem('evmAT', JSON.stringify(payload));
+    setAddressList(oldPayload.address.map(item => (item === address ? newAddress : item)));
+  };
 
   return (
     <div>
-      {/*
-        ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
-      */}
-      <div style={{ border: '1px solid #cccccc', padding: 16, width: 400, margin: 'auto', marginTop: 64 }}>
+      <h2 className="text-xl text-center font-medium text-gray-900 dark:text-white">EVM Address Tracker</h2>
+      <div style={{ border: '1px solid #cccccc', padding: 16, width: 400, margin: 'auto', marginTop: 30 }}>
         <h2>Add Address:</h2>
         <Divider />
         <div style={{ margin: 8 }}>
           <Input
+            value={newAddress}
             onChange={e => {
               setNewAddress(e.target.value);
+              // clear input after
+              if (addressList.includes(newAddress)) {
+                setNewAddress('');
+              }
             }}
           />
           <Button
             style={{ marginTop: 8 }}
             onClick={async () => {
+              // verify address is not already added
+              if (addressList.includes(newAddress)) {
+                alert('Address already added');
+                setNewAddress('');
+                return;
+              }
+              // verify valid ethereum address
+              if (!utils.isAddress(newAddress)) {
+                alert('Invalid Ethereum Address');
+                return;
+              }
+
+              // Add Address
               if (localStorage?.getItem('evmAT')) {
                 const oldPayload = JSON.parse(localStorage.getItem('evmAT'));
-                const payload = { address: [...oldPayload[address], newAddress] };
-                localStorage.setItem('evmAT', JSON.stringify(payload));
+                console.log('view old payload ', oldPayload[address]);
+                oldPayload[address] = [...oldPayload[address], newAddress];
+                localStorage.setItem('evmAT', JSON.stringify(oldPayload));
+                setAddressList([...newAddress, addressList]);
+                setNewAddress('');
               } else {
-                const payload = { address: [newAddress] };
-                localStorage.setItem('evmAT', JSON.stringify(payload));
+                const newItem = {};
+                newItem[address] = [newAddress];
+                localStorage.setItem('evmAT', JSON.stringify(newItem));
+                setAddressList([newAddress]);
+                setNewAddress('');
               }
               console.log('newAddress', newAddress);
             }}
@@ -58,156 +119,45 @@ export default function ExampleUI({
           </Button>
         </div>
         <Divider />
-        <h2>Remove Address:</h2>
-        <Divider />
-        <Divider />
-        <h2>Edit Address:</h2>
-        <Divider />
-        <Divider />
         <h2>View All Addresses:</h2>
         <Divider />
-        {JSON.stringify(addressList)}
+        {addressList && addressList.length > 0 ? (
+          addressList.map((address, index) => {
+            return (
+              address && (
+                <div className="text-black" key={index}>
+                  <AddressListDisplay address={address} ensProvider={mainnetProvider} fontSize={16} />
+                  <Balance address={address} provider={mainnetProvider} price={price} />
+                  ETH
+                  <Button
+                    style={{ marginTop: 8 }}
+                    onClick={() => {
+                      removeAddress(address);
+                    }}
+                  >
+                    Remove
+                  </Button>
+                  <>
+                    <Button onClick={() => toggleEdit(address)}>Edit</Button>
+                    <Input value={address} hidden={!editStates[address]} />
+                    <Button
+                      onClick={() => {
+                        editAddress(address, newAddress);
+                      }}
+                      hidden={!editStates[address]}
+                    >
+                      Save Edit
+                    </Button>
+                  </>
+                  <Divider />
+                </div>
+              )
+            );
+          })
+        ) : (
+          <p>No Addresses Added</p>
+        )}
         <Divider />
-      </div>
-
-      {/*
-        ⚙️ Here is an example UI that displays and sets the purpose in your smart contract:
-      */}
-      <div style={{ border: '1px solid #cccccc', padding: 16, width: 400, margin: 'auto', marginTop: 64 }}>
-        <h2>Example UI:</h2>
-        <h4>purpose: {purpose}</h4>
-        <Divider />
-        <Divider />
-        Your Address:
-        <Address address={address} ensProvider={mainnetProvider} fontSize={16} />
-        <Divider />
-        ENS Address Example:
-        <Address
-          address="0x34aA3F359A9D614239015126635CE7732c18fDF3" /* this will show as austingriffith.eth */
-          ensProvider={mainnetProvider}
-          fontSize={16}
-        />
-        <Divider />
-        {/* use utils.formatEther to display a BigNumber: */}
-        <h2>Your Balance: {yourLocalBalance ? utils.formatEther(yourLocalBalance) : '...'}</h2>
-        <div>OR</div>
-        <Balance address={address} provider={localProvider} price={price} />
-        <Divider />
-        <div>🐳 Example Whale Balance:</div>
-        <Balance balance={utils.parseEther('1000')} provider={localProvider} price={price} />
-        <Divider />
-        {/* use utils.formatEther to display a BigNumber: */}
-        <h2>Your Balance: {yourLocalBalance ? utils.formatEther(yourLocalBalance) : '...'}</h2>
-        <Divider />
-        Your Contract Address:
-        <Address
-          address={readContracts && readContracts.YourContract ? readContracts.YourContract.address : null}
-          ensProvider={mainnetProvider}
-          fontSize={16}
-        />
-        <Divider />
-        <div style={{ margin: 8 }}>
-          <Button
-            onClick={() => {
-              /* look how you call setPurpose on your contract: */
-              tx(writeContracts.YourContract.setPurpose('🍻 Cheers'));
-            }}
-          >
-            Set Purpose to &quot;🍻 Cheers&quot;
-          </Button>
-        </div>
-        <div style={{ margin: 8 }}>
-          <Button
-            onClick={() => {
-              /*
-              you can also just craft a transaction and send it to the tx() transactor
-              here we are sending value straight to the contract's address:
-            */
-              tx({
-                to: writeContracts.YourContract.address,
-                value: utils.parseEther('0.001'),
-              });
-              /* this should throw an error about "no fallback nor receive function" until you add it */
-            }}
-          >
-            Send Value
-          </Button>
-        </div>
-        <div style={{ margin: 8 }}>
-          <Button
-            onClick={() => {
-              /* look how we call setPurpose AND send some value along */
-              tx(
-                writeContracts.YourContract.setPurpose('💵 Paying for this one!', {
-                  value: utils.parseEther('0.001'),
-                }),
-              );
-              /* this will fail until you make the setPurpose function payable */
-            }}
-          >
-            Set Purpose With Value
-          </Button>
-        </div>
-        <div style={{ margin: 8 }}>
-          <Button
-            onClick={() => {
-              /* you can also just craft a transaction and send it to the tx() transactor */
-              tx({
-                to: writeContracts.YourContract.address,
-                value: utils.parseEther('0.001'),
-                data: writeContracts.YourContract.interface.encodeFunctionData('setPurpose(string)', [
-                  '🤓 Whoa so 1337!',
-                ]),
-              });
-              /* this should throw an error about "no fallback nor receive function" until you add it */
-            }}
-          >
-            Another Example
-          </Button>
-        </div>
-      </div>
-
-      <div style={{ width: 600, margin: 'auto', marginTop: 32, paddingBottom: 256 }}>
-        <Card style={{ marginTop: 32 }}>
-          <div>
-            There are tons of generic components included from{' '}
-            <a href="https://ant.design/components/overview/" target="_blank" rel="noopener noreferrer">
-              🐜 ant.design
-            </a>{' '}
-            too!
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            <Button type="primary">Buttons</Button>
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            <SyncOutlined spin /> Icons
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            Date Pickers?
-            <div style={{ marginTop: 2 }}>
-              <DatePicker onChange={() => {}} />
-            </div>
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Slider range defaultValue={[20, 50]} onChange={() => {}} />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Switch defaultChecked onChange={() => {}} />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Progress percent={50} status="active" />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Spin />
-          </div>
-        </Card>
       </div>
     </div>
   );
